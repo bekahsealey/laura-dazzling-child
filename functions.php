308f54be2ca42745df595df30b062ebb7549c541
bekahsealey <bekah@bekahsealey.com>
2016-04-22 18:23:29 -0500

    register submenu

--- functions.php
+++ functions.php
@@ -4,4 +4,28 @@ function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
 }
+
+add_action( 'after_setup_theme', 'register_submenu' );
+function register_submenu() {
+  register_nav_menu( 'submenu', __( 'Submenu', 'dazzling' ) );
+}
+
+/**
+ * sub menu (should you choose to use one)
+ */
+function dazzling_sub_menu() {
+  // display the WordPress Custom Menu if available
+  wp_nav_menu(array(
+    'menu'              => 'submenu',
+    'theme_location'    => 'submenu',
+    'depth'             => 2,
+    'container'         => 'div',
+    'container_class'   => 'collapse navbar-collapse navbar-ex1-collapse',
+    'menu_class'        => 'nav navbar-nav',
+    'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+    'walker'            => new wp_bootstrap_navwalker()
+  ));
+} /* end sub menu */
+
+
 ?>
