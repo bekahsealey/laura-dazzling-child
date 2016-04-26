70e6667945e6b6258ffdb2b9a4998a83e7c987a0
bekahsealey <bekah@bekahsealey.com>
2016-04-26 13:42:23 -0500

    structured for flexbox centered top menu

--- header.php
+++ header.php
@@ -24,7 +24,7 @@
 
 	<nav class="navbar navbar-default" role="navigation">
 		<div class="container">
-			<div class="navbar-header">
+			<div class="navbar-header flex">
 			  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
 			    <span class="sr-only"><?php _e( 'Toggle navigation', 'dazzling' ); ?></span>
 			    <span class="icon-bar"></span>
@@ -43,20 +43,20 @@
 				<?php if( !get_header_image() ) : ?>
 
 					<div id="logo">
-						<span class="site-title"><a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span>
+						<p class="site-title" ><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
 					</div><!-- end of #logo -->
             <?php $description = get_bloginfo( 'description', 'display' );
-            if ( $description || is_customize_preview() ) : ?>
+            	if ( $description || is_customize_preview() ) : ?>
                     <p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-            <?php
-            endif; ?>
+            <?php endif; ?>
 
 				<?php endif; // header image was removed (again) ?>
 
 			</div>
 
-				<?php dazzling_header_menu(); ?>
 
+				<?php dazzling_left_menu(); ?>
+				<?php dazzling_right_menu(); ?>
 		</div>
 	</nav><!-- .site-navigation -->
 		<div class="meta">
@@ -71,7 +71,7 @@
 		<?php dazzling_call_for_action(); ?>
         </div>
 
-        		<?php dazzling_sub_menu(); ?>
+				<?php dazzling_header_menu(); ?>
 
         <div id="content" class="site-content container">
 
