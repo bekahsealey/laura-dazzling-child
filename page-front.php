75ef9f863e8836402dd7ce8c58a1aa1030eb2fed
bekahsealey <bekah@bekahsealey.com>
2016-04-27 12:30:04 -0500

    structured front page with testimonials outside main container

--- page-front.php
+++ page-front.php
@@ -8,7 +8,18 @@
  */
 
 get_header(); ?>
-	<div id="primary" class="content-area col-sm-12 col-md-12">
+<div class="meta">
+	<aside class="intro">
+			<?php the_meta(); ?>
+	</aside>
+</div>
+<div class="top-section">
+	<?php dazzling_featured_slider(); ?>
+	<?php dazzling_call_for_action(); ?>
+	<?php dazzling_submenu(); ?>
+</div>
+<aside class="testimonial">	
+	<div class="container">
 		<?php $args = array(
 			'post_type' => 'lauras_testimonials',
 			'orderby'        => 'rand',
@@ -18,13 +29,39 @@ get_header(); ?>
 		if ( $my_query->have_posts() ) { 
 			while ( $my_query->have_posts() ) {
 				$my_query->the_post();
-				$do_not_duplicate = get_the_ID();
-				the_post_thumbnail( 'thumbnail' );
-				the_content();
+				$do_not_duplicate = get_the_ID(); ?>
+				<div class="col-md-4"> 
+				<?php the_post_thumbnail( 'testimonial' ); ?>
+				</div>
+				<div class="col-md-8 quote">
+				<?php the_content(); ?>
+				</div>
+				<?php
+
 			}
 		}
 		wp_reset_postdata();
 		?>
+	</div>
+</aside>
+
+        <div id="content" class="site-content container">
+
+            <div class="container main-content-area"><?php
+
+                global $post;
+                if( get_post_meta($post->ID, 'site_layout', true) ){
+                        $layout_class = get_post_meta($post->ID, 'site_layout', true);
+                }
+                else{
+                        $layout_class = of_get_option( 'site_layout' );
+                }
+                if( is_home() && is_sticky( $post->ID ) ){
+                        $layout_class = of_get_option( 'site_layout' );
+                }
+                ?>
+                <div class="row <?php echo $layout_class; ?>">
+	<div id="primary" class="content-area col-sm-12 col-md-12">
 		<main id="main" class="site-main" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
 
@@ -33,22 +70,40 @@ get_header(); ?>
 			<?php endwhile; // end of the loop. ?>
 			
 		</main><!-- #main -->
+
+	</div><!-- #primary -->
+
+                </div><!-- close .row -->
+            </div><!-- close .container -->
+        </div><!-- close .site-content -->
+
+<aside class="testimonial">	
+	<div class="container">
 		<?php $args = array(
 			'post_type' => 'lauras_testimonials',
 			'post__not_in' => array( $do_not_duplicate ),
 			'orderby'        => 'rand',
 			'posts_per_page' => '1',
-			);
+		);
 		$my_query = new WP_Query( $args );
 		if ( $my_query->have_posts() ) { 
 			while ( $my_query->have_posts() ) {
-				$my_query->the_post();
-				the_post_thumbnail( 'thumbnail' );
-				the_content();
+				$my_query->the_post(); ?>
+				<div class="col-md-8 quote"> 
+				<?php the_content(); ?>
+				</div>
+				<div class="col-md-4">
+				<?php the_post_thumbnail( 'testimonial' ); ?>
+				</div>
+				<?php
 			}
 		}
 		?>
+	</div>
+</aside>
 
+<div class="container">
+<div class="col-md-6">
 <?php $args = array(
 	'post_type' => 'lauras_podcasts',
 	'posts_per_page' => '5',
@@ -61,10 +116,11 @@ $my_query = new WP_Query( $args );
 		}
 	}
 ?>	
-
+</div>
+<div class="col-md-6">
 <?php $args = array(
 	'post_type' => 'lauras_qanda',
-	'posts_per_page' => '5',
+	'posts_per_page' => '1',
 	);
 $my_query = new WP_Query( $args );
 	if ( $my_query->have_posts() ) {
@@ -75,7 +131,8 @@ $my_query = new WP_Query( $args );
 		}
 	}
 ?>
+</div>
+</div>
 
-	</div><!-- #primary -->
 
 <?php get_footer(); ?>
